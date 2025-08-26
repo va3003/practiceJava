d29fd8ba31cc66f6dad98a3e8de065fcb360da22
vch4pl-prog <vch4pl@gmail.com>
2025-08-25 21:35:26 -0400

    Update ArrayPractice.java

--- src/main/java/com/Learning/practiceJava/ArrayPractice.java
+++ src/main/java/com/Learning/practiceJava/ArrayPractice.java
@@ -9,7 +9,7 @@ public class ArrayPractice {
 		ArrayPractice.countLettersInString(str);
 		System.out.println("Reverse String : " + ArrayPractice.reverseString(str));
 
-		String str2 = "hello bachho where are you a9 for rebase?";
+		String str2 = "hello bachho where are you a10 for rebase vch?";
 		ArrayPractice.reverseNonSpacedString(str2);
 		String str3 = "hello";
 	}
